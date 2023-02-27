e83d197f122d5b145a4686522edd6b83fdbf9b0c
Ágabo Pinho <3427129+agabopinho@users.noreply.github.com>
2023-02-27 10:06:49 -0300

    feat: atualizado script único

--- azure/build.sh
+++ azure/build.sh
@@ -1,13 +1,20 @@
 group="quantumleap"
 acr="quantumleap"
-log="quantumleap"
-envname="quantumleap"
+logsworkspace="quantumleap"
+appenv="quantumleap"
 
+echo "creating resource group"
 az group create -n $group -l brazilsouth 
+
+echo "creating acr"
 az acr create -n $acr -g $group --sku Basic
 az acr identity assign -n $acr -g $group --identities [system]
-az monitor log-analytics workspace create -n $log -g $group 
-az containerapp env create -n $envname -g $group \
-    --logs-workspace-id $(az monitor log-analytics workspace show -n $log -g $group --query customerId | tr -d \"-) \
-    --logs-workspace-key $(az monitor log-analytics workspace get-shared-keys -n $log -g $group --query primarySharedKey | tr -d \") \
+
+echo "creating log-analytics workspace"
+az monitor log-analytics workspace create -n $logsworkspace -g $group 
+
+echo "creating containerapp env"
+az containerapp env create -n $appenv -g $group \
+    --logs-workspace-id $(az monitor log-analytics workspace show -n $logsworkspace -g $group --query customerId | tr -d \"-) \
+    --logs-workspace-key $(az monitor log-analytics workspace get-shared-keys -n $logsworkspace -g $group --query primarySharedKey | tr -d \") \
     -l brazilsouth
